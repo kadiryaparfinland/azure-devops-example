9685ea3434bd81a0802fcb4dd235a87d237cffb6
andromegasoftware <kadiryapar16@gmail.com>
2023-01-30 11:59:00 +0200

    import code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int total = num1 + num2;
+
+            Console.WriteLine("The total is: " + total);
+        }
+    }
